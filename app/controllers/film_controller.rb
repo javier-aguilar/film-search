5fb35905ec7ae9c701215905d8d9e4c2178bfb50
Javier Aguilar <jvr.agx@gmail.com>
2021-06-12 14:19:03 -0600

    Add error response method

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -1,7 +1,8 @@
 class FilmController < ApplicationController
   def search
-    data = Film.search(params[:query])
+    return error('Query required', 400) unless params[:query]
 
+    data = Film.search(params[:query])
     render json: { data: data }, status: 200
   end
 end

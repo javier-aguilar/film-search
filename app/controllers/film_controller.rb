a98cb311838be341341b08666cd1d5cf79974c70
Javier Aguilar <jvr.agx@gmail.com>
2021-06-16 11:14:14 -0600

    Check for capitalization in query

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -2,7 +2,7 @@ class FilmController < ApplicationController
   def search
     return error('Query required', 400) unless params[:query].present?
 
-    data = Film.search(params[:query])
+    data = Film.search(params[:query].downcase)
     render json: { data: data }, status: 200
   end
 end
--- spec/api/film/film_spec.rb
+++ spec/api/film/film_spec.rb
@@ -28,35 +28,34 @@ RSpec.describe 'Films', type: :request do
       get '/film/search?query=alien'
 
       expect(response).to be_successful
-      json = JSON.parse(response.body, symbolize_names: true)
-      data = json[:data]
+      json_1 = JSON.parse(response.body, symbolize_names: true)
+      data_1 = json_1[:data]
 
-      result = Search.last
-      expect(result[:query]).to eq 'alien'
-      expect(result[:count]).to eq 2
+      result_1 = Search.last
+      expect(result_1[:query]).to eq 'alien'
+      expect(result_1[:count]).to eq 2
 
-      expect(data).to be_an Array
-      expect(data).not_to be_empty
-      expect(data[0]).to have_key :id
-      expect(data[0]).to have_key :title
-      expect(data[0]).to have_key :year
-    end
-    it 'returns film(s) from db with capitalization' do
+      expect(data_1).to be_an Array
+      expect(data_1).not_to be_empty
+      expect(data_1[0]).to have_key :id
+      expect(data_1[0]).to have_key :title
+      expect(data_1[0]).to have_key :year
+
       get '/film/search?query=Alien'
 
       expect(response).to be_successful
-      json = JSON.parse(response.body, symbolize_names: true)
-      data = json[:data]
+      json_2 = JSON.parse(response.body, symbolize_names: true)
+      data_2 = json_2[:data]
 
-      result = Search.last
-      expect(result[:query]).to eq 'Alien'
-      expect(result[:count]).to eq 3
+      result_2 = Search.last
+      expect(result_2[:query]).to eq 'alien'
+      expect(result_2[:count]).to eq 3
 
-      expect(data).to be_an Array
-      expect(data).not_to be_empty
-      expect(data[0]).to have_key :id
-      expect(data[0]).to have_key :title
-      expect(data[0]).to have_key :year
+      expect(data_2).to be_an Array
+      expect(data_2).not_to be_empty
+      expect(data_2[0]).to have_key :id
+      expect(data_2[0]).to have_key :title
+      expect(data_2[0]).to have_key :year
     end
   end
   describe 'GET film/search?query=' do

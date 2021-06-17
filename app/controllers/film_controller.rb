bda8997c10277f49403acf8596650acff2ee924a
Javier Aguilar <jvr.agx@gmail.com>
2021-06-17 07:56:53 -0600

    Check for sort_by and order params

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -2,7 +2,19 @@ class FilmController < ApplicationController
   def search
     return error('Query required', 400) unless params[:query].present?
 
-    data = Film.search(params[:query].downcase)
-    render json: { data: data }, status: 200
+    results = Film.search(params[:query].downcase)
+    if params[:sort_by] == 'title' || params[:sort_by] == 'year'
+      sort_results(results, params[:sort_by])
+      results.reverse! if params[:order] == 'desc'
+    else
+      results
+    end
+    render json: { data: results }, status: 200
+  end
+
+  private
+
+  def sort_results(results, key)
+    results.sort_by! { |film| film[key].downcase }
   end
 end

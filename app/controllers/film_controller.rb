38da802923124ea75b60887f284e5592812012ab
Javier Aguilar <jvr.agx@gmail.com>
2021-06-17 12:23:37 -0600

    Move sort logic to Film class

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -2,19 +2,13 @@ class FilmController < ApplicationController
   def search
     return error('Query required', 400) unless params[:query].present?
 
-    results = Film.search(params[:query].downcase)
+    results = Film.search(params[:query].downcase, params[:filter])
+
     if params[:sort_by] == 'title' || params[:sort_by] == 'year'
-      sort_results(results, params[:sort_by])
-      results.reverse! if params[:order] == 'desc'
+      Film.sort_results(results, params[:sort_by], params[:order])
     else
       results
     end
     render json: { data: results }, status: 200
-  end
-
-  private
-
-  def sort_results(results, key)
-    results.sort_by! { |film| film[key].downcase }
   end
 end
